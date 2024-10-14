615424902e2c677dc88068d93cc2aa0143f69db1
zmannoham <143953699+zmannoham@users.noreply.github.com>
2024-10-14 01:16:33 -0400

    Added more comments on the rsync command.

--- run_rclone.sh
+++ run_rclone.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Run rclone sync
-rclone sync -vv ":sftp:${SFTP_PATH}" "gcstest:${GCS_BUCKET}${GCS_PATH}" \
+## :sftp: uses generic SFTP backend. We just supply the host, user, pass, and port via flags.
+## gcscon: uses the GCS backend that we defined in the rclone.conf config file. This is the only way I could get it to work.
+## gcs-bucket-policy-only is needed when uniform access control on a bucket is enabled compared to ACLs. Otherwise this may need to be changed.
+rclone sync -vv ":sftp:${SFTP_PATH}" "gcscon:${GCS_BUCKET}${GCS_PATH}" \
     --sftp-host=${SFTP_HOST} \
     --sftp-user=${SFTP_USERNAME} \
     --sftp-pass=$(rclone obscure "${SFTP_PASSWORD}") \
